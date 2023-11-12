8daac92a5081d45c8ffeb34183a96585a4e15b8c
linafq <furukawaqueiroz@gmail.com>
2023-11-12 18:12:24 -0300

    lógicas de loop

--- escritas.py
+++ escritas.py
@@ -5,6 +5,8 @@ from filtrapalavras import filtra
 from filtrapalavras import lista_atualizada as listat
 from indicaposicaocorreta import inidica_posicao
 from inicializatermo import inicializa
+from inicializatermo import sorteada
+
 print( '===========================')
 print('|                           |')
 print('| Bem-vindo ao Insper Termo |')
@@ -27,4 +29,49 @@ print('Sorteando uma palavra...')
 print('Já tenho uma palavra! Tente adivinhá-la!')
 print(' ')
 
-resultado = inidica_posicao()
+
+tentativas = 6 
+palavras_coloridas = []
+acertou = 0
+
+while tentativas > 0: 
+    print ('Você tem {0} tentativa(s)!'.format(tentativas))
+    especulada = input('Qual seu palpite?')
+    print('Insper :: TERMO')
+
+    if especulada == sorteada:
+        acertou = 1
+        break 
+    if especulada == 'desisto' :
+        break 
+
+    position = inidica_posicao(sorteada, especulada)
+    
+    letras_cores = []
+
+    count = 0
+
+    for posicao in position:
+        if posicao == 2:
+            letras_cores.append(especulada[count])
+        
+        elif posicao == 1: 
+            letra_amarela = f' {Fore.YELLOW}(especulada[count]){Style.RESET_ALL}'
+            letras_cores.append(letra_amarela)
+       
+        else:
+            letra_azul = f'{Fore.BLUE}(especulada[count]{Style.RESET_ALL}'
+            letras_cores.append(letra_azul)
+            
+        count += 1
+
+
+    palavras_coloridas.append(letras_cores)
+    print(palavras_coloridas)
+    tentativas -= 1
+
+    if acertou == 1: 
+        print('Uhull você acertou !!')
+    else: 
+        print('Não foi dessa vez...')
+        print(' A palavra sorteada era {0}!'.format(sorteada))
